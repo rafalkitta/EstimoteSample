d2004e3de28933c14f29906368d76a0b3258c5f2
rafalkitta <rafalkit@gmail.com>
2014-03-07 15:43:07 +0100

    Initial Commit

---
+++ EstimoteSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EstimoteSample
+//
+//  Created by Rafał Kitta on 07.03.2014.
+//  Copyright (c) 2014 Rafał Kitta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EstimoteSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EstimoteSample
+//
+//  Created by Rafał Kitta on 07.03.2014.
+//  Copyright (c) 2014 Rafał Kitta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
